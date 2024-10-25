19cbf67517b997ae0f868ac7a2d2bf679bca9d05
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-10-25 11:01:28 -0500

    Creacion del modelo Ingredient

---
+++ app/Models/Ingredient.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingredient extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+
+    public function pizzas()
+    {
+        return $this->belongsToMany(Pizza::class, 'pizza_ingredient');
+    }
+}
